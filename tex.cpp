9e2b4383f13c8a561ff1b68329e997ff05572cfe
stillsaiko <stillsaiko@gmail.com>
2019-07-27 14:26:38 +0200

    Update tex.cpp

--- tex.cpp
+++ tex.cpp
@@ -1,26 +1,6 @@
 # include "tex.h"
 # include <cassert>
 # define GL_ASSERT assert(glGetError( ) == GL_NO_ERROR);
-// NEW
-template<TEXTARGET> struct TEXDIM ;
-template<> struct TEXDIM<1D>					{ typedef TEX1D type ; };
-
-template<> struct TEXDIM<2D>					{ typedef TEX2D type ; };
-template<> struct TEXDIM<1D_ARRAY>				{ typedef TEX2D type ; };
-template<> struct TEXDIM<2D_MULTISAMPLE>		{ typedef TEX2D type ; };
-
-template<> struct TEXDIM<3D>					{ typedef TEX3D type ; };
-template<> struct TEXDIM<2D_ARRAY>				{ typedef TEX3D type ; };
-template<> struct TEXDIM<CUBE_MAP>				{ typedef TEX3D type ; };
-template<> struct TEXDIM<CUBE_MAP_ARRAY>		{ typedef TEX3D type ; };
-template<> struct TEXDIM<2D_MULTISAMPLE_ARRAY>	{ typedef TEX3D type ; };
-
-template<TEXTARGET T> struct foo : TEXDIM<T>::type {
-};
-foo<CUBE_MAP> xxxxxx ;
-struct TEX2D_MUTLISAMPLE_ARRAY {
-	
-};
 // ...
 constexpr GLenum TEXBINDING(GLenum target){ // TEXTARGET
 	switch(target){
@@ -80,12 +60,11 @@ void TEX1D::resize(GLsizei X){
 	glGetTexLevelParameteriv(T, 0, GL_TEXTURE_INTERNAL_FORMAT, &i);	GL_ASSERT
 	glTexImage1D(T, LOD, i, X, 0, format(i), type(i), nullptr);		GL_ASSERT
 }
-// protected
 TEX1D::~TEX1D(void){
 	if(ID) glDeleteTextures(1u, &ID);
 }
 TEX1D::TEX1D(void){ }
-TEX1D::TEX1D(TEXTARGET T, GLenum i, GLsizei X, GLvoid * PX){
+TEX1D::TEX1D(GLenum T, GLenum i, GLsizei X, GLvoid * PX){
 	this->T = T ;
 	glGenTextures(1u, &ID);											GL_ASSERT
 	image(i, X, PX);
@@ -113,12 +92,11 @@ void TEX2D::resize(GLsizei X, GLsizei Y){
 	glTexImage2D(T, LOD, i, X, Y, 0, format(i), type(i), nullptr);	GL_ASSERT
 	glBindTexture(T, B);											GL_ASSERT
 }
-// protected
 TEX2D::~TEX2D(void){
 	if(ID) glDeleteTextures(1u, &ID);
 }
 TEX2D::TEX2D(void){ }
-TEX2D::TEX2D(TEXTARGET T, GLenum i, GLsizei X, GLsizei Y, GLvoid * PX){
+TEX2D::TEX2D(GLenum T, GLenum i, GLsizei X, GLsizei Y, GLvoid * PX){
 	this->T = T ;													GL_ASSERT
 	glGenTextures(1u, &ID);											GL_ASSERT
 	image(i, X, Y, PX); //	AO = ::TEX<2D>(GL_DEPTH_COMPONENT16, 720, 720);
@@ -146,12 +124,11 @@ void TEX3D::resize(GLsizei X, GLsizei Y, GLsizei Z){
 	glTexImage3D(T, LOD, i, X, Y, Z, 0, format(i), type(i), nullptr);GL_ASSERT
 	glBindTexture(T, B);											GL_ASSERT
 }
-// protected
 TEX3D::~TEX3D(void){
 	if(ID) glDeleteTextures(1u, &ID);
 }
 TEX3D::TEX3D(void){ }
-TEX3D::TEX3D(TEXTARGET T, GLenum i, GLsizei X, GLsizei Y, GLsizei Z, GLvoid * PX){
+TEX3D::TEX3D(GLenum T, GLenum i, GLsizei X, GLsizei Y, GLsizei Z, GLvoid * PX){
 	this->T = T ;
 	glGenTextures(1u, &ID);											GL_ASSERT
 	image(i, X, Y, Z, PX);
@@ -159,61 +136,3 @@ TEX3D::TEX3D(TEXTARGET T, GLenum i, GLsizei X, GLsizei Y, GLsizei Z, GLvoid * PX
 TEX3D& TEX3D::operator = (TEX3D&& a)noexcept {
 	ID = a.ID; T = a.T; a.ID = 0; a.T = 0;	return *this ;
 }
-/*
-// template specialization __________________________________________________
-// n-dimensional ______________________________________________________
-TEX<1D>::TEX(GLenum F, GLsizei X, GLvoid* P): TEX1D(1D, F, X, P){ }
-TEX<1D>::TEX(void){ }
-TEX<1D>& TEX<1D>::operator = (TEX<1D>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-TEX<2D>::TEX(GLenum F, GLsizei X, GLsizei Y, GLvoid* P): TEX2D(2D, F, X, Y, P){ }
-TEX<2D>::TEX(void){ }
-TEX<2D>&
-TEX<2D>::operator = (TEX<2D>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-TEX<3D>::TEX(GLenum F, GLsizei X, GLsizei Y, GLsizei Z, GLvoid* P): TEX3D(3D, F, X, Y, Z, P){ }
-TEX<3D>::TEX(void){ }
-TEX<3D>&
-TEX<3D>::operator = (TEX<3D>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-// array ________________________________________________________
-TEX<1D_ARRAY>::TEX(GLenum F, GLsizei X, GLsizei Y, GLvoid* P): TEX2D(1D_ARRAY, F, X, Y, P){ }
-TEX<1D_ARRAY>::TEX(void){ }
-TEX<1D_ARRAY>&
-TEX<1D_ARRAY>::operator = (TEX<1D_ARRAY>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-TEX<2D_ARRAY>::TEX(GLenum F, GLsizei X, GLsizei Y, GLsizei Z, GLvoid* P): TEX3D(2D_ARRAY, F, X, Y, Z, P){ }
-TEX<2D_ARRAY>::TEX(void){ }
-TEX<2D_ARRAY>&
-TEX<2D_ARRAY>::operator = (TEX<2D_ARRAY>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-// rect ____________________________________________________________________________
-// ________________________________ R E C T A N G L E ______________________________
-// _________________________________________________________________________________
-TEX<RECTANGLE>::TEX(GLenum F, GLsizei X, GLsizei Y, GLvoid* P): TEX2D(RECTANGLE, F, X, Y, P){ }
-TEX<RECTANGLE>::TEX(void){ }
-TEX<RECTANGLE>&
-TEX<RECTANGLE>::operator = (TEX<RECTANGLE>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-// cube ____________________________________________________________________________
-// _________________________________ C U B E _ M A P _______________________________
-// _________________________________________________________________________________
-TEX<CUBE_MAP>::TEX(GLenum F, GLsizei X, GLsizei Y, GLvoid* P): TEX2D(CUBE_MAP, F, X, Y, P){ }
-TEX<CUBE_MAP>::TEX(void){ }
-TEX<CUBE_MAP>&
-TEX<CUBE_MAP>::operator = (TEX<CUBE_MAP>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-// array ________________________________________________________
-TEX<CUBE_MAP_ARRAY>::TEX(GLenum F, GLsizei X, GLsizei Y, GLsizei Z, GLvoid* P): TEX3D(CUBE_MAP_ARRAY, F, X, Y, Z, P){ }
-TEX<CUBE_MAP_ARRAY>::TEX(void){ }
-TEX<CUBE_MAP_ARRAY>&
-TEX<CUBE_MAP_ARRAY>::operator = (TEX<CUBE_MAP_ARRAY>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-// multisample _____________________________________________________________________
-// _______________________________ M U L T I S A M P L E ___________________________
-// _________________________________________________________________________________
-TEX<2D_MULTISAMPLE>::TEX(GLenum F, GLsizei X, GLsizei Y, GLvoid* P): TEX2D(2D_MULTISAMPLE, F, X, Y, P){ }
-TEX<2D_MULTISAMPLE>::TEX(void){ }
-TEX<2D_MULTISAMPLE>&
-TEX<2D_MULTISAMPLE>::operator = (TEX<2D_MULTISAMPLE>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-TEX<2D_MULTISAMPLE_ARRAY>::TEX(GLenum F, GLsizei X, GLsizei Y, GLsizei Z, GLvoid* P): TEX3D(2D_MULTISAMPLE_ARRAY, F, X, Y, Z, P){ }
-TEX<2D_MULTISAMPLE_ARRAY>::TEX(void){ }
-TEX<2D_MULTISAMPLE_ARRAY>&
-TEX<2D_MULTISAMPLE_ARRAY>::operator = (TEX<2D_MULTISAMPLE_ARRAY>&& a)noexcept { ID = a.ID; T = a.T; a.ID = 0; a.T = 0; return *this ; }
-// buffer __________________________________________________________________________
-// ___________________________________ B U F F E R _________________________________
-// _________________________________________________________________________________
-TEX<BUFFER>::TEX(void){ assert(false); }
-TEX<BUFFER>::TEX(GLenum internalformat, GLuint buffer){ assert(false); }*/
--- tex.h
+++ tex.h
@@ -1,37 +1,6 @@
 # pragma once
 # include "fbo.h"
 # define GL_ASSERT assert(glGetError( ) == GL_NO_ERROR);
-// GLenum target ________________________________________________
-enum TEXTARGET : GLenum {
-	RECTANGLE		= GL_TEXTURE_RECTANGLE,
-	CUBE_MAP		= GL_TEXTURE_CUBE_MAP,
-	CUBE_MAP_ARRAY	= GL_TEXTURE_CUBE_MAP_ARRAY,
-	BUFFER			= GL_TEXTURE_BUFFER
-};
-// illegal literals (warning C4455)
-# pragma warning(suppress: 4455)
-constexpr TEXTARGET operator "" D(unsigned long long n){
-	if( n == 1 )return (TEXTARGET) GL_TEXTURE_1D ;
-	if( n == 2 )return (TEXTARGET) GL_TEXTURE_2D ;
-	if( n == 3 )return (TEXTARGET) GL_TEXTURE_3D ;
-	return (TEXTARGET) -1 ; // error
-}
-# pragma warning(suppress: 4455)
-constexpr TEXTARGET operator "" D_ARRAY(unsigned long long n){
-	if( n == 1 )return (TEXTARGET) GL_TEXTURE_1D_ARRAY ;
-	if( n == 2 )return (TEXTARGET) GL_TEXTURE_2D_ARRAY ;
-	return (TEXTARGET) -1 ; // error
-}
-# pragma warning(suppress: 4455)
-constexpr TEXTARGET operator "" D_MULTISAMPLE(unsigned long long n){
-	if( n == 2 )return (TEXTARGET) GL_TEXTURE_2D_MULTISAMPLE ;
-	return (TEXTARGET) -1 ; // error
-}
-# pragma warning(suppress: 4455)
-constexpr TEXTARGET operator "" D_MULTISAMPLE_ARRAY(unsigned long long n){
-	if( n == 2 )return (TEXTARGET) GL_TEXTURE_2D_MULTISAMPLE_ARRAY ;
-	return (TEXTARGET) -1 ; // error
-}
 // NEW
 struct TEX : public FBO::A {
 	void bind(GLenum = GL_TEXTURE0);
@@ -43,102 +12,33 @@ protected:
 struct TEX1D : public TEX {
 	void image(GLenum format, GLsizei width, GLvoid * pixels);
 	void resize(GLsizei width);
-//protected:
 	~
 	TEX1D(void);
 	TEX1D(void);
-	TEX1D(TEXTARGET, GLenum, GLsizei, GLvoid*);
+	TEX1D(GLenum target, GLenum format, GLsizei width, GLvoid * pixels = nullptr);
 	TEX1D& operator = (TEX1D&&)noexcept ;
 };
 struct TEX2D : public TEX {
 	void image(GLenum format, GLsizei width, GLsizei height, GLvoid * pixels);
-	void resize(GLsizei width, GLsizei height);
-//protected:
+	void resize(GLsizei width,
+				GLsizei height);
 	~
 	TEX2D(void);
 	TEX2D(void);
-	TEX2D(TEXTARGET, GLenum, GLsizei, GLsizei, GLvoid*);
+	TEX2D(GLenum target, GLenum format,	GLsizei width, 
+										GLsizei height, GLvoid * pixels = nullptr);
 	TEX2D& operator = (TEX2D&&)noexcept ;
 };
 struct TEX3D : public TEX {
-	void image(GLenum format, GLsizei width, GLsizei height, GLsizei depth, GLvoid* pixels);
-	void resize(GLsizei width, GLsizei height, GLsizei depth);
-//protected:
+	void image(GLenum format, GLsizei width, GLsizei height, GLsizei depth, GLvoid * pixels);
+	void resize(GLsizei width,
+				GLsizei height,
+				GLsizei depth);
 	~
 	TEX3D(void);
 	TEX3D(void);
-	TEX3D(TEXTARGET, GLenum, GLsizei, GLsizei, GLsizei, GLvoid*);
+	TEX3D(GLenum target, GLenum format,	GLsizei width,
+										GLsizei height,
+										GLsizei depth, GLvoid * pixels = nullptr);
 	TEX3D& operator = (TEX3D&&)noexcept ;
 };
-/*
-// template prototype __________________________________________________
-template<TEXTARGET>struct TEX ;
-// n-dimensional ______________________________________________________
-template<> struct TEX<1D> : public TEX1D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLvoid* pixels=nullptr);
-	TEX<1D>& operator = (TEX<1D>&& a)noexcept ;
-};
-template<> struct TEX<2D> : public TEX2D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLsizei height, GLvoid* pixels=nullptr);
-	TEX<2D>& operator = (TEX<2D>&& a)noexcept ;
-};
-template<> struct TEX<3D> : public TEX3D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLsizei height, GLsizei depth, GLvoid* pixels=nullptr);
-	TEX<3D>& operator = (TEX<3D>&& a)noexcept ;
-};
-// array ________________________________________________________
-template<> struct TEX<1D_ARRAY> : public TEX2D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLsizei height, GLvoid* pixels=nullptr);
-	TEX<1D_ARRAY>& operator = (TEX<1D_ARRAY>&& a)noexcept ;
-};
-template<> struct TEX<2D_ARRAY> : public TEX3D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLsizei height, GLsizei depth, GLvoid* pixels=nullptr);
-	TEX<2D_ARRAY>& operator = (TEX<2D_ARRAY>&& a)noexcept ;
-};
-// rect ____________________________________________________________________________
-// ________________________________ R E C T A N G L E ______________________________
-// _________________________________________________________________________________
-template<> struct TEX<RECTANGLE> : public TEX2D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLsizei height, GLvoid* pixels=nullptr);
-	TEX<RECTANGLE>& operator = (TEX<RECTANGLE>&& a)noexcept ;
-};
-// cube ____________________________________________________________________________
-// _________________________________ C U B E _ M A P _______________________________
-// _________________________________________________________________________________
-template<> struct TEX<CUBE_MAP> : public TEX2D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLsizei height, GLvoid* pixels=nullptr);
-	TEX<CUBE_MAP>& operator = (TEX<CUBE_MAP>&& a)noexcept ;
-};
-// array ________________________________________________________
-template<> struct TEX<CUBE_MAP_ARRAY> : public TEX3D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLsizei height, GLsizei depth, GLvoid* pixels=nullptr);
-	TEX<CUBE_MAP_ARRAY>& operator = (TEX<CUBE_MAP_ARRAY>&& a)noexcept ;
-};
-// multisample _____________________________________________________________________
-// _______________________________ M U L T I S A M P L E ___________________________
-// _________________________________________________________________________________
-template<> struct TEX<2D_MULTISAMPLE> : public TEX2D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLsizei height, GLvoid* pixels=nullptr);
-	TEX<2D_MULTISAMPLE>& operator = (TEX<2D_MULTISAMPLE>&& a)noexcept ;
-};
-template <> struct TEX<2D_MULTISAMPLE_ARRAY> : public TEX3D {
-	TEX(void);
-	TEX(GLenum format, GLsizei width, GLsizei height, GLsizei depth, GLvoid* pixels=nullptr);
-	TEX<2D_MULTISAMPLE_ARRAY>& operator = (TEX<2D_MULTISAMPLE_ARRAY>&& a)noexcept ;
-};
-// buffer __________________________________________________________________________
-// ___________________________________ B U F F E R _________________________________
-// _________________________________________________________________________________
-template<> struct TEX<BUFFER> : public TEXBASE {
-	TEX(void);
-	TEX(GLenum internalformat, GLuint buffer);
-};*/
