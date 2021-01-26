edb1e7f9cc697bd0d259fb4d386199f85b57264a
stillsaiko <stillsaiko@gmail.com>
2021-01-26 01:34:20 +0100

    Update hid.h

--- hid.h
+++ hid.h
@@ -1,3 +1,4 @@
+# pragma once
 /*# ifdef __MINGW32__
 # define _WIN32_WINNT _WIN32_WINNT_WIN7
 //#   define _WIN32_WINNT 0x0501
@@ -30,39 +31,48 @@
 //#	include <ddk/hidsdi.h>
 # endif // __MINGW32__
 template<class = void> struct HID;
+// HID <struct {char [2], BTN, X[2], Y[2], Z[2], [2];}>	(hWnd, 0xFF00, 0x000A) // wacom
+// HID <struct {char ID, [2], X, Y, BTN, LR;}>		(hWnd, 0x0001, 0x0004) // nintendo
+// HID <RAWKEYBOARD>	(hWnd, 0x0001, 0x0006) // keyboard
+// HID <RAWMOUSE>	(hWnd, 0x0001, 0x0002) // mouse
 template<> struct HID<void>
 {
-	virtual ~
+	const DWORD dwCount;
+	~
 	HID(void);
 	HID(void);
-	HID(USHORT usUsagePage, USHORT usUsage, HWND hWnd);
-	size_t operator( )(WPARAM W, LPARAM L); // returns access length
-	operator void *(void);
+	HID(HWND hWnd, USHORT usUsagePage, USHORT usUsage, DWORD dwFlags = 0);
+	void operator =(HID<void> &&) noexcept;
+	HANDLE operator( )(WPARAM W, LPARAM L); // returns access length
+	operator void *(void) const;
 protected:
-	std::set<HANDLE> set;
-	RAWINPUTDEVICE RIDEV {0};
-	DWORD RIM_TYPE = RIM_TYPEHID;
-	size_t size = 0u;
-	char * data = nullptr;
+	HANDLE *hDevice = nullptr;
+	RAWINPUTDEVICE RAWINPUTDEVICE { };
+	DWORD dwType = 0;
+	RAWINPUT *RAWINPUT = nullptr;
+	UINT cbSize = 0;
 };
 template<class T> struct HID : public HID<void>
 {
-	HID(void){ }
-	HID(USHORT usUsagePage, USHORT usUsage, HWND hWnd):
-	HID<void>(usUsagePage, usUsage, hWnd){ }
-	void operator = (HID<T> && refref) noexcept
+	HID(void) = default;
+//	HID(HID<T> &&) = default;
+	HID(HWND hWnd, USHORT usUsagePage, USHORT usUsage, DWORD dwFlags = 0):
+	HID<void>(hWnd, usUsagePage, usUsage, dwFlags)
 	{
-		RIDEV = refref.RIDEV ;
-		size = refref.size ; size = 0u ;
-		data = refref.data ; data = nullptr ;
-		set = std::move(refref.set);
+		if(dwType == RIM_TYPEMOUSE) ASSERT(sizeof(T) == sizeof(RAWMOUSE));
+		if(dwType == RIM_TYPEKEYBOARD) ASSERT(sizeof(T) == sizeof(RAWKEYBOARD));
 	}
-	T * operator -> (void)
+//	void operator =(HID<T> &&)
+	const T *operator ->(void) const
 	{
-		return reinterpret_cast<T *>(operator void *( ));
+		return static_cast<const T*>(operator void *( ));
 	}
-	T & operator[ ](size_t index)
+	const T &operator *(void) const
 	{
-		return reinterpret_cast<T *>(operator void *( ))[index];
+		return *static_cast<const T*>(operator void *( ));
+	}
+	const T &operator[ ](size_t n) const
+	{
+		return static_cast<const T*>(operator void *( ))[n];
 	}
 };
